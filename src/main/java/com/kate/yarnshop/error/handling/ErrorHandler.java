ed2807a163cf4ae3d01a3612d5f12672a571f961
dethdenona@gmai.com <d1amondEyes>
2021-03-08 18:41:49 +0200

    add exception handler

---
+++ src/main/java/com/kate/yarnshop/error/handling/ErrorHandler.java
@@ -0,0 +1,26 @@
+package com.kate.yarnshop.error.handling;
+
+import com.kate.yarnshop.exceptions.EntityAlreadyExistsException;
+import com.kate.yarnshop.exceptions.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class ErrorHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<Object> handleEntityNotFound(Exception ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(EntityAlreadyExistsException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ResponseEntity<Object> handleConflict(Exception ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
+    }
+}
